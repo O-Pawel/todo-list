d139db415c4e45ed707aa809cd484cc5e5c9e99b
Pawel Olszewski <opawel@protonmail.com>
2023-03-12 22:13:58 +0100

    added new tasks and showing if a task is done, added list.css

--- js/script.js
+++ js/script.js
@@ -15,8 +15,10 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
-                ${task.content}
+            <li 
+            class="list__item ${task.done ? "list__item--done" : ""}"  
+            >
+            ${task.content}    
             </li>
             `;
         }
@@ -24,8 +26,32 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+    
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
