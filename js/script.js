79dd17275f4943e02141489d1ee1a6ae512155bf
Pawel Olszewski <opawel@protonmail.com>
2023-03-12 22:40:10 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,22 +10,6 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li 
-            class="list__item ${task.done ? "list__item--done" : ""}"  
-            >
-            ${task.content}    
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -34,18 +18,50 @@
         render();
     };
 
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
+    
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li 
+            class="list__item ${task.done ? "list__item--done" : ""}"  
+            >
+            <button class="js-remove">Usuń zadanie</button>
+            ${task.content}    
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+    
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-        
+
         if (newTaskContent === "") {
             return;
         }
 
         addNewTask(newTaskContent);
     };
-    
+
     const init = () => {
         render();
 
