dadf0c777044704445b205003266e1b59149d382
Pawel Olszewski <opawel@protonmail.com>
2023-03-12 20:54:51 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+{
+    const tasks = [
+        {
+            content: "zadanie 1",
+            done: false,
+        },
+        {
+            content: "zadanie 2",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
